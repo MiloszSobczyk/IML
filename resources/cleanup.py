67846f881cf7ce32b277f9c7e72a95a3bbd59ae3
MiloszSobczyk <m.sobczyk.official@gmail.com>
2024-10-26 22:36:41 +0200

    Update cleanup.py

--- resources/cleanup.py
+++ resources/cleanup.py
@@ -3,7 +3,7 @@ import os
 def removeHiddenFiles(rootDir):
     for dirPath, _, filenames in os.walk(rootDir):
         for fileName in filenames:
-            if fileName.startswith('.'):
+            if fileName.startswith('.') and 'git' not in fileName:
                 filePath = os.path.join(dirPath, fileName)
                 os.remove(filePath)
                 print(filePath)
