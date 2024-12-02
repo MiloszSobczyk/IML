5e9bdd73361c44c0af8d9a180be49faea792a75f
MiloszSobczyk <m.sobczyk.official@gmail.com>
2024-12-02 17:12:35 +0100

    GGEZ

---
+++ network/models/model1.py
@@ -0,0 +1,30 @@
+import torch.nn as nn
+
+class SimpleCNN(nn.Module):
+    def __init__(self, num_classes=9):
+        super(SimpleCNN, self).__init__()
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, padding=1) 
+        self.act1 = nn.ReLU() 
+        self.pool1 = nn.MaxPool2d(2)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
+        self.act2 = nn.ReLU()
+        self.pool2 = nn.MaxPool2d(2)  # --> (45,45)
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
+        self.act3 = nn.ReLU()
+        self.pool3 = nn.MaxPool2d(2) # --> (22,22)
+        self.fc1 = nn.Linear(128 * 22 * 22 , 256)  
+        self.act4 = nn.ReLU()
+        self.dropout=nn.Dropout(p=0.2) 
+        self.fc2 = nn.Linear(256, 9) 
+
+
+    def forward(self, x):
+        out = self.pool1(self.act1(self.conv1(x)))
+        out = self.pool2(self.act2(self.conv2(out)))
+        out = self.pool3(self.act3(self.conv3(out)))
+        out = out.view(out.size(0), -1)
+        out = self.act4(self.fc1(out))
+        out=self.dropout(out)
+        out=self.fc2(out)
+        
+        return out
